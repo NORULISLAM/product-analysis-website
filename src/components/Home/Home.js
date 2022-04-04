1c021e10a83c534657c50348debd396f0059ad6b
user <juyelr16@gmail.com>
2022-04-04 11:37:22 +0900

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="mx-auto">
+                    <a class="navbar-brand" href="/">HOME</a>
+                    <a class="navbar-brand" href="/">REVIEWS</a>
+                    <a class="navbar-brand" href="/">DASHBORD</a>
+                    <a class="navbar-brand" href="/">BLOGS</a>
+                    <a class="navbar-brand" href="/">ABOUT</a>
+
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Home;
